4348af96698b409488811578e7dab587918a7786
Chak Nanga <cnanga@gmail.com>
2023-08-04 14:26:09 -0700

    Added support for API, Storage and Auth

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,1 +1,38 @@
-export type AmplifyDependentResourcesAttributes = {}
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "photoop": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string"
+    }
+  },
+  "auth": {
+    "photoopaf2c8847af2c8847": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    },
+    "userPoolGroups": {
+      "MediaBuyersGroupRole": "string",
+      "MediaCreatorsGroupRole": "string"
+    }
+  },
+  "function": {
+    "photoopaf2c8847af2c8847PostConfirmation": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    }
+  },
+  "storage": {
+    "UserCreatedMedia": {
+      "BucketName": "string",
+      "Region": "string"
+    }
+  }
+}
